3284986535092810551897e4152ce31a3247e5c2
MuhammedSoliu <muhammedsoliu2222@gmail.com>
2023-01-12 23:36:19 -0800

    task-2

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -4,4 +4,6 @@ def replace_in_list(my_list, idx, element):
     if idx < 0 or idx >= len(my_list):
         return my_list
     else:
-        return my_list.insert(idx, element)
+        my_list[idx] = element
+
+        return my_list

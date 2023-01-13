5579120b28dc247d8f039786facdbba4e2632217
MuhammedSoliu <muhammedsoliu2222@gmail.com>
2023-01-12 23:34:02 -0800

    task-2

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
 
 def replace_in_list(my_list, idx, element):
-    for i in range(len(my_list)):
-        if idx != i:
-            return my_list
-        else:
-            my_list.insert(idx, element)
-
-            return my_list
+    if idx < 0 or idx >= len(my_list):
+        return my_list
+    else:
+        return my_list.insert(idx, element)

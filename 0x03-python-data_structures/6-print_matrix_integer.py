78cdb90248f73ecbf968fa756ec58e54d8f495d0
MuhammedSoliu <muhammedsoliu2222@gmail.com>
2023-01-13 04:24:22 -0800

    task-6

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -4,8 +4,8 @@ def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         for column in row:
             if column == row[(len(row) - 1)]:
-                print("{:d}".format(column), end= '')
+                print("{:d}".format(column), end='')
             else:
-                print("{:d}".format(column),end=' ')
+                print("{:d}".format(column), end=' ')
 
         print()

556a32309b341e49fd7857840845907ad3296695
MuhammedSoliu <muhammedsoliu2222@gmail.com>
2023-01-12 23:25:15 -0800

    task-2

---
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def replace_in_list(my_list, idx, element):
+    for i in range(len(my_list)):
+        if idx != i:
+            return my_list
+        else:
+            my_list.insert(idx, element)
+
+    return my_list

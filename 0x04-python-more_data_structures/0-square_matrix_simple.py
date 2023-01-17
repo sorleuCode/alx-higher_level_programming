05b4ac867e94428dd0dffb28e17e9d598735f73d
MuhammedSoliu <muhammedsoliu2222@gmail.com>
2023-01-17 00:27:45 -0800

    task-0

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 
 def square_matrix_simple(matrix=[]):
-    new_matrix = matrix.copy()
-    for row in new_matrix:
+    for row in matrix:
         return [list(map(lambda col: col ** 2, row))]

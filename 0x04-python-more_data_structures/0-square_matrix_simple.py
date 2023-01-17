64256179c0d1bface13c00de1685606ebfa12e31
MuhammedSoliu <muhammedsoliu2222@gmail.com>
2023-01-17 00:31:49 -0800

    task-0

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 
 def square_matrix_simple(matrix=[]):
-    for row in matrix:
-        return [list(map(lambda col: col ** 2, row))]
+        return [list(map(lambda col: col ** 2, row)) for row in matrix]
